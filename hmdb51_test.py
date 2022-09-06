e96d021c170c8e7587e2a9bab32573fd70fb1684
haosome6 <zhzjim@gmail.com>
2022-09-05 23:10:17 -0400

    add dataloader

--- hmdb51_test.py
+++ hmdb51_test.py
@@ -1,5 +1,22 @@
-import torch, torchvision
+import torch, torchvision, time
+from datetime import timedelta
+from torchvision import transforms
+from torch import nn
 
-hmdb51 = torchvision.datasets.HMDB51('./hmdb51/', './testTrainMulti_7030_splits/', 5, 5)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-print(hmdb51[0])
+transform = transforms.Compose([transforms.Lambda(lambda x: x / 255.),
+                                transforms.Resize((240, 320))])
+
+hmdb51 = torchvision.datasets.HMDB51('./hmdb51/', './testTrainMulti_7030_splits/', 16, 5, transform=transform)
+
+hmdb51_loader = torch.utils.data.DataLoader(hmdb51, batch_size=1, shuffle=True)
+
+for i, (data,_, target) in enumerate(hmdb51_loader):
+    data = data.to(device)
+    target = target.to(device)
+    print(data.shape)
+    print(i)
+
+
+
--- ucf101_test.py
+++ ucf101_test.py
@@ -1,18 +1,36 @@
 import torch, torchvision, time
+from datetime import timedelta
+from torchvision import transforms
+from torch import nn
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-ucf101 = torchvision.datasets.UCF101('./ucf101/', './ucfTrainTestlist/', 5, 5)
+tfs = transforms.Compose([
+            # TODO: this should be done by a video-level transfrom when PyTorch provides transforms.ToTensor() for video
+            # scale in [0, 1] of type float
+            transforms.Lambda(lambda x: x / 255.),
+            # reshape into (T, C, H, W) for easier convolutions
+            transforms.Lambda(lambda x: x.permute(0, 3, 1, 2)),
+            # rescale to the most common size
+            transforms.Lambda(lambda x: nn.functional.interpolate(x, (240, 320)))])
 
-ucf101_loader = torch.utils.data.DataLoader(ucf101, batch_size=256, shuffle=True, num_workers=1)
+ucf101 = torchvision.datasets.UCF101('./ucf101/', './ucfTrainTestlist/', frames_per_clip=16, step_between_clips=2, fold=1, train=True, transform=tfs)
 
-start = time.time()
-print(start)
+def custom_collate(batch):
+    filtered_batch = []
+    for video, _, label in batch:
+        filtered_batch.append((video, label))
+    return torch.utils.data.dataloader.default_collate(filtered_batch)
 
-for i, (data, target) in enumerate(ucf101_loader):
+
+ucf101_loader = torch.utils.data.DataLoader(ucf101, batch_size=16, shuffle=True, collate_fn=custom_collate)
+
+start_time = time.monotonic()
+
+for i, (data,_, target) in enumerate(ucf101_loader):
     data = data.to(device)
     target = target.to(device)
     print(i)
 
-end = time.time()
-print(end - start)
+end_time = time.monotonic()
+print(timedelta(seconds=end_time - start_time))
