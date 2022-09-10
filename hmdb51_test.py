09cc63bbcb7b7c071e87148c333bf65b2817ed43
haosome6 <zhzjim@gmail.com>
2022-09-09 23:44:57 -0400

    update test process

--- hmdb51_test.py
+++ hmdb51_test.py
@@ -2,17 +2,21 @@ import torch, torchvision, time
 from datetime import timedelta
 from torchvision import transforms
 from torch import nn
+import pdb
+
+torch.manual_seed(0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-transform = transforms.Compose([transforms.Lambda(lambda x: x / 255.),
-                                transforms.Resize((240, 320))])
+hmdb51 = torchvision.datasets.HMDB51('/mnt/c/Users/zhz/course_code/dataset_test/hmdb51/', '/mnt/c/Users/zhz/course_code/dataset_test/testTrainMulti_7030_splits/', 16, 5)
+torch.save(hmdb51, '/mnt/c/Users/zhz/course_code/dataset_test/hmdb51_cache.pth')
 
-hmdb51 = torchvision.datasets.HMDB51('./hmdb51/', './testTrainMulti_7030_splits/', 16, 5, transform=transform)
+# hmdb51 = torch.load('/mnt/c/Users/zhz/course_code/dataset_test/hmdb51_cache.pth')
 
 hmdb51_loader = torch.utils.data.DataLoader(hmdb51, batch_size=1, shuffle=True)
 
 for i, (data,_, target) in enumerate(hmdb51_loader):
+    # pdb.set_trace()
     data = data.to(device)
     target = target.to(device)
     print(data.shape)
--- ucf101_test.py
+++ ucf101_test.py
@@ -3,27 +3,13 @@ from datetime import timedelta
 from torchvision import transforms
 from torch import nn
 
+torch.manual_seed(0)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-tfs = transforms.Compose([
-            # TODO: this should be done by a video-level transfrom when PyTorch provides transforms.ToTensor() for video
-            # scale in [0, 1] of type float
-            transforms.Lambda(lambda x: x / 255.),
-            # reshape into (T, C, H, W) for easier convolutions
-            transforms.Lambda(lambda x: x.permute(0, 3, 1, 2)),
-            # rescale to the most common size
-            transforms.Lambda(lambda x: nn.functional.interpolate(x, (240, 320)))])
+ucf101 = torchvision.datasets.UCF101('./ucf101/', './ucfTrainTestlist/', frames_per_clip=16, step_between_clips=2, fold=1, train=True)
 
-ucf101 = torchvision.datasets.UCF101('./ucf101/', './ucfTrainTestlist/', frames_per_clip=16, step_between_clips=2, fold=1, train=True, transform=tfs)
-
-def custom_collate(batch):
-    filtered_batch = []
-    for video, _, label in batch:
-        filtered_batch.append((video, label))
-    return torch.utils.data.dataloader.default_collate(filtered_batch)
-
-
-ucf101_loader = torch.utils.data.DataLoader(ucf101, batch_size=16, shuffle=True, collate_fn=custom_collate)
+ucf101_loader = torch.utils.data.DataLoader(ucf101, batch_size=16, shuffle=True)
 
 start_time = time.monotonic()
 
